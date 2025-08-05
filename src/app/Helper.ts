036f5451df86011577ef59cd703ed34c52cf1bfd
jlariv11 <jlariv95@gmail.com>
2025-08-04 22:50:05 -0400

    remove debug code

--- src/app/Helper.ts
+++ src/app/Helper.ts
@@ -4,7 +4,6 @@ export function itemsAsGroupedArray(items: Item[]) {
     const groupedItems: Item[][] = [];
     for(const item of items){
         if(item.alternateForId){
-            console.log("skipping", item.itemName, item.alternateForId)
             continue;
         }
         const alternates = items.filter(i => i.alternateForId === item.itemID);
