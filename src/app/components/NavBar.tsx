d511aebfa2ed6b722eec4f4373729c9b2c9fdac9
Jake Lariviere <jlariv95@gmail.com>
2025-11-02 15:42:34 -0500

    update nav bar name

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -14,8 +14,8 @@ export default function NavBar({session}: NavBarProps) {
         <>
             <div className={"w-full bg-gray-400 p-4 border-gray-300 border-4 rounded-xl"}>
                 <div className={"flex justify-between"}>
-                    <div className={"flex space-x-4 text-2xl cursor-pointer"}>
-                        <div onClick={() => router.push("/CreateList")} className={"pt-2"}>My List Maker: BEYOND</div>
+                    <div className={"flex space-x-4 text-4xl cursor-pointer"}>
+                        <div onClick={() => router.push("/CreateList")} className={"pt-2"}>Gift List</div>
                     </div>
                     <div>
                         {session ? <Image className={"hover:cursor-pointer"} onClick={() => router.push("/AccountPage")} src={session.user.picture || ""} alt={"User Icon"} width={50} height={50}/> : <ListButton buttonText={"Log In"} onClick={() => window.location.href = "auth/login"} />}
