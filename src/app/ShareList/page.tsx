2618246ffe4732ede6f68420ea322c731f5123a2
Jake Lariviere <jlariv95@gmail.com>
2025-11-02 00:54:44 -0400

    view page revamp
    revamp share page

--- src/app/ShareList/page.tsx
+++ src/app/ShareList/page.tsx
@@ -6,6 +6,8 @@ import {loadViewList} from "../database/LoadList";
 import ItemElement from "../elements/ItemElement";
 import {useSearchParams} from "next/navigation";
 import ReviewPurchases from "@/app/components/ReviewPurchases";
+import WarningModal from "@/app/components/WarningModal";
+import Modal from "@/app/components/Modal";
 
 export interface SimpleItem {
     itemID: number | undefined;
@@ -39,9 +41,12 @@ export default function ShareList() {
         const item = items.find(i => i.itemID === itemID);
         const localItem = purchasedItems.find(i => i.itemID === itemID);
         if(item && localItem) {
-            localItem.itemQuantityPurchased = Math.min(quantity - (item.itemQuantityPurchased || 0), item.itemQuantity);
-            if(localItem.itemQuantityPurchased <= 0) {
-                setPurchasedItems(purchasedItems.filter(i => i.itemID !== itemID));
+            const newQuantity = Math.min(quantity - (item.itemQuantityPurchased || 0), item.itemQuantity);
+
+            if (newQuantity <= 0) {
+                setPurchasedItems(prev => prev.filter(i => i.itemID !== itemID));
+            } else {
+                setPurchasedItems(prev => prev.map(i => i.itemID === itemID ? { ...i, itemQuantityPurchased: newQuantity } : i));
             }
         }else if(item){
             const localItem = {itemID: item.itemID, itemName: item.itemName, itemQuantityPurchased: quantity - (item.itemQuantityPurchased || 0)};
@@ -49,6 +54,7 @@ export default function ShareList() {
         }
     }
 
+    //TODO: fix visual bug when an item is fully purchased another item's purchase count will be changed visually
     function saveItems(){
         purchasedItems.forEach((localItem) => {
             const item = items.find(i => i.itemID === localItem.itemID);
@@ -66,16 +72,31 @@ export default function ShareList() {
             <div className={`m-2 px-4 sm:px-6 md:px-8 flex flex-col items-center transition-all duration-100 ${showModal ? "blur-xs" : ""}`}>
                 <div className={"w-full max-w-4xl space-y-1.5"}>
                     <h1 className={"text-4xl"}>View {listName}</h1>
-                    <div className={"h-108 overflow-y-auto border-4 rounded-md border-gray-400 p-4 bg-gray-300"}>
-                        {items.filter(i => i.itemQuantity !== i.itemQuantityPurchased).map((item, index) => <ItemElement key={index} item={item} updatePurchaseCount={updatePurchaseCount}/>)}
-                    </div>
-                    <div>
-                        <ListButton onClick={() => setShowModal(true)} buttonText={'Review Purchases'}/>
+                    <div className={"flex gap-2"}>
+                        <div className={"w-2/3 h-150 overflow-y-auto border-4 rounded-md border-gray-400 p-4 bg-gray-300"}>
+                            {items.filter(i => i.itemQuantity !== i.itemQuantityPurchased).map((item, index) => <ItemElement key={index} item={item} updatePurchaseCount={updatePurchaseCount}/>)}
+                        </div>
+                        <div className="w-1/3 h-150 border-4 rounded-md border-gray-400 ml-2 p-4 bg-gray-300 flex flex-col justify-between">
+                            <div>
+                                <h1 className={"text-xl font-bold"}>Purchases</h1>
+                                {purchasedItems.map((item, index) => (
+                                    <div key={index} className={"pb-2"}>
+                                        <h1 className={"text-lg"}>{item.itemName} x {item.itemQuantityPurchased}</h1>
+                                        <hr />
+                                    </div>
+                                ))}
+                            </div>
+
+                            <ListButton
+                                onClick={() => setShowModal(true)}
+                                buttonText="Confirm Purchases"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
             <div id={"modal"}></div>
-            {showModal && <ReviewPurchases setShowReview={setShowModal} modalDivID={"modal"} saveItems={saveItems} items={purchasedItems}/>}
+            {showModal && <Modal modalTitle={"Purchase these Items?"} modalDivID={"modal"} modalBody={<WarningModal toggleModal={setShowModal} warning={"This will mark these items as purchased and will make these items unavailable to purchase by others. Do you want to continue?"} actionFunction={saveItems} />} showModalToggle={setShowModal} />}
         </div>
 
     )
