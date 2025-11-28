f28ac17c246b9df70e9c73a4758e1962708b1190
Jake Lariviere <jlariv95@gmail.com>
2025-11-28 13:36:58 -0500

    make cookies more browser friendly

--- src/app/Cookies.ts
+++ src/app/Cookies.ts
@@ -1,6 +1,10 @@
 export function setCookie(name: string, value: string, days: number) {
     const expires = new Date(Date.now() + days * 864e5).toUTCString();
-    document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
+    document.cookie =
+        `${name}=${encodeURIComponent(value)}; ` +
+        `expires=${expires}; ` +
+        `path=/; ` +
+        `SameSite=Lax`;
 }
 
 export function getCookie(name: string): string | null {
